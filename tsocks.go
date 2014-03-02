117124fcc4c1bbc3dc8ddc3733082e42a27a29e3
Brandon Fouts <brandon@brandonfouts.com>
2014-03-02 01:22:45 -0500

    cleanup for personal use

--- tsocks.go
+++ tsocks.go
@@ -1,14 +1,14 @@
 package gotsocks
 
 import (
-	"fmt"
 	"bytes"
-	"time"
-	"strings"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
-	"io/ioutil"
+	"strings"
+	"time"
 )
 
 const (
@@ -19,52 +19,53 @@ const (
 var (
 	ConfigLocation = `/tmp/tsocks-%s:%s.conf`
 	PasswordConfig = `
-local = 192.168.0.0/255.255.0.0
-local = 127.0.0.1/255.0.0.0
-local = %s.0.0.0/255.0.0.0
+local = 192.168.0.0/255.255.255.0
+local = 10.0.0.0/255.0.0.0
+local = %s/255.255.255.255
 server = %s
 server_type = %d
 server_port = %s
 default_user = %s
 default_pass = %s`
-	Config         = `
-local = 192.168.0.0/255.255.0.0
-local = 127.0.0.1/255.0.0.0
-local = %s.0.0.0/255.0.0.0
+	Config = `
+local = 192.168.0.0/255.255.255.0
+local = 10.0.0.0/255.0.0.0
+local = %s/255.255.255.255
 server = %s
 server_type = %d
 server_port = %s`
 )
 
 type Proxy struct {
-	IP         string
-	Port       string
-	User       string
-	Password   string
-	Version    int
+	IP       string
+	Port     string
+	User     string
+	Password string
+	Version  int
 }
 
 //proxy format: user:password@ip:port
 //version:Socks5 or Socks4
 func New(proxy string, version int) (*Proxy, error) {
 	temp := strings.FieldsFunc(proxy, func(r rune) bool {
-			switch r{
-			case '@', ':':
-				return true
-			}
-			return false
-		})
-	switch len(temp){
+		switch r {
+		case '@', ':':
+			return true
+		}
+		return false
+	})
+	switch len(temp) {
 	case 4:
-		p := &Proxy{User:temp[0], Password:temp[1], IP:temp[2], Port:temp[3], Version:version}
+		p := &Proxy{User: temp[0], Password: temp[1], IP: temp[2], Port: temp[3], Version: version}
 		return p, p.sure()
 	case 2:
-		p := &Proxy{IP:temp[0], Port:temp[1], Version:version}
+		p := &Proxy{IP: temp[0], Port: temp[1], Version: version}
 		return p, p.sure()
 
 	}
 	return nil, errors.New(fmt.Sprintf("Malformat proxy %s\n", proxy))
 }
+
 //execute command with tsocks
 //timeout in seconds
 func (proxy *Proxy) Command(name string, timeout int64, arg ...string) ([]byte, error) {
@@ -79,12 +80,12 @@ func (proxy *Proxy) Command(name string, timeout int64, arg ...string) ([]byte, 
 	}
 	c := make(chan error)
 	go func() {
-		c<- cmd.Wait()
+		c <- cmd.Wait()
 	}()
 	select {
 	case res := <-c:
 		err = res
-	case <-time.After(time.Duration(timeout)*time.Second):
+	case <-time.After(time.Duration(timeout) * time.Second):
 		cmd.Process.Kill()
 		err = errors.New(fmt.Sprintf("%s timeout", name))
 		return nil, err
@@ -99,8 +100,12 @@ func (proxy *Proxy) configFile() string {
 
 func exists(path string) bool {
 	_, err := os.Stat(path)
-	if err == nil { return true }
-	if os.IsNotExist(err) { return false }
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
 	return false
 }
 
@@ -110,9 +115,9 @@ func (proxy *Proxy) sure() error {
 		var content string
 		ps := strings.Split(proxy.IP, ".")
 		if proxy.User == "" {
-			content = fmt.Sprintf(Config, ps[0], proxy.IP, proxy.Version, proxy.Port)
-		}else {
-			content = fmt.Sprintf(PasswordConfig, ps[0], proxy.IP, proxy.Version, proxy.Port, proxy.User, proxy.Password)
+			content = fmt.Sprintf(Config, ps, proxy.IP, proxy.Version, proxy.Port)
+		} else {
+			content = fmt.Sprintf(PasswordConfig, ps, proxy.IP, proxy.Version, proxy.Port, proxy.User, proxy.Password)
 		}
 		return ioutil.WriteFile(f, []byte(content), 0644)
 	}
